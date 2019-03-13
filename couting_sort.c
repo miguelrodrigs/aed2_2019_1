46e5526afa1e2565e105366a4ea609c30cd576f1
cetdo <ceto.snf17@uea.edu.br>
2019-03-13 17:41:49 -0400

    Colocando o 'counting_sort.c' no padrão pedido pelo professor mesmo assim o algoritmo está errado ou não executando como desejado, nçao ordenando os 100 números

--- couting_sort.c
+++ couting_sort.c
@@ -1,34 +1,42 @@
-#include<stdio.h>
- 
-void counting_sort(int a[],int n,int max)
+#include "io.h"
+/*
+void max_element(int vector[], int size)
 {
-    int count[50]={0},i,j;
-
-    for(i=0;i<n;++i)
-        count[a[i]]=count[a[i]]+1;
-
-    printf("\nElementos Ordenados: ");
-
-    for(i=0;i<=max;++i)
-        for(j=1;j<=count[i];++j)
-            printf("%d ",i);
+  int max = 0;
+  for int (i = 0; i < size; i++)
+  {
+    if (max < vector[i])
+      max = vector[i];
+  }
 }
- 
+*/
+void counting_sort(int vector[], int size)
+{
+    int count[size];
+    int i,j;
+
+    for(i = 0; i < size; ++i)
+        count[vector[i]] = count[vector[i]] + 1;
+
+}
+
 int main()
 {
-    int a[50],n,i,max=0;
+    /*
+    int a[50], n, i, max = 0;
     printf("Insira a quantidade de elementos: ");
-    scanf("%d",&n);
-                      
-    for(i=0;i<n;++i)
+    scanf("%d", &n);
+
+    for( i =0; i < n; ++i)
     {
         printf("Insira o numero: ");
-        scanf("%d",&a[i]);
-        if(a[i]>max)
-            max=a[i];
+        scanf("%d", &a[i]);
+        if(a[i] > max)
+            max = a[i];
     }
-     
-    counting_sort(a,n,max);
+
+    counting_sort(a, n, max);
     printf("\n");
     return 0;
+    */
 }
