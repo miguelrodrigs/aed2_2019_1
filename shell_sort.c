0694865d87b3b9050b2c102929c23c40c8cd55d9
cetdo <ceto.snf17@uea.edu.br>
2019-03-13 22:00:32 -0400

    arquivo 'shell_sort.c' agora nos padrões acordados em sala

--- shell_sort.c
+++ shell_sort.c
@@ -1,51 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-#define MAX 10
- 
+#include "io.h"
+
 void shell_sort(int *a, int size);
- 
-int main(int argc, char** argv)
+
+void shell_sort(int *vector, int size)
 {
- int i, vet[MAX];
- 
- for(i = 0; i < MAX; i++)
- {
-  printf("Digite um valor: ");
-  scanf("%d", &vet[i]);
- }
- 
- shell_sort(vet, MAX);
- 
- printf("Valores ordenados: "); 
- for(i = 0; i < MAX; i++)
- {
- 	printf("%d, ", vet[i]);
- }
- 	printf("\n");
- return 0;
-}
- 
-void shell_sort(int *a, int size)
-{
- int i , j , value;
+ int i, j, value;
  int gap = 1;
-  
- do {
-  gap = 3*gap+1;
- } while(gap < size);
-  
- do {
-  gap /= 3;
-  for(i = gap; i < size; i++) {
-   value = a[i];
-   j = i - gap;
-    
-   while (j >= 0 && value < a[j]) {
-    a[j + gap] = a[j];
-    j -= gap;
-   }
-   a[j + gap] = value;
+
+ do
+ {
+   gap = 3 * gap + 1;
+ }while(gap < size);
+
+ do
+ {
+   gap /= 3;
+   for(i = gap; i < size; i++)
+   {
+     value = vector[i];
+     j = i - gap;
+
+     while (j >= 0 && value < vector[j])
+     {
+       vector[j + gap] = vector[j];
+       j -= gap;
+     }
+
+     vector[j + gap] = value;
   }
  }while(gap > 1);
 }
+/*
+int main()
+{
+  int vector[TAM];
+  create_vector(vector, TAM);
+  shell_sort(vector, TAM);
+  print_vector(vector,TAM);
+
+  return 0;
+}
+*/
