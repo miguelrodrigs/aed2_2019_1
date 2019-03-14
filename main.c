a3a651efdd9ec9f004d57adba3bf006df4d8c626
cetdo <ceto.snf17@uea.edu.br>
2019-03-13 21:51:49 -0400

    Deixando o "selection_sort.c" de acordo com o solicitado em sala

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include "io.h"
 #include "bubble_sort.c"
+#include "selection_sort.c"
 #define TAM 1000
 
 int main(){
   int vector[TAM];
   create_vector(vector, TAM);
-  bubble_sort(vector, TAM);
+  selection_sort(vector, TAM);
   print_vector(vector, TAM);
 }
--- selection_sort.c
+++ selection_sort.c
@@ -1,69 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+void selection_sort(int *vector, int size);
 
-#define TAM 1000
-
-//void povoa_vetor(int *vetor);
-//void imprime_vetor(int *vetor);
-void selection_sort(int *vetor);
-/*
-int main()
-{
-    int vetor[TAM];
+int selection_sort(int *vector, int size){
+    int smallest;
     int aux;
+    int aux_vect[size] = vector;
 
-    povoa_vetor(vetor);
-    selection_sort(vetor);
-    imprime_vetor(vetor);
-
-    system("pause");
-    return 0;
-}
-
-void povoa_vetor(int *vetor)
-{
-    int aux;
-    srand ( time(NULL) );
-    for ( aux = 0; aux < TAM; aux++)
-        {
-        vetor[aux] = (rand() % 89) + 10;
-        printf(" %d,",vetor[aux]);
-        }
-}
-
-void imprime_vetor(int *vetor)
-{
-    int aux;
-    printf("\n\n");
-    for ( aux = 0; aux < TAM; aux++)
+    for( int i = 0; i < size-1; i++)
     {
-        printf(" %d,", vetor[aux]);
-    }
-    printf("\n\n ");
+        smallest = i;
 
-}
-*/
-int selection_sort(int *vetor){
-    int menor;
-    int aux;
-    int aux_vet[TAM] = vetor;
-
-    for( int i = 0; i < TAM-1; i++)
-    {
-        menor = i;
-
-        for (int j = i+1; j < TAM; j++)
+        for (int j = i+1; j < size; j++)
         {
-            if (aux_vet[j] < aux_vet[menor])
+            if (aux_vect[j] < aux_vect[smallest])
             {
-                menor = j;
+                smallest = j;
             }
         }
-        aux = aux_vet[i];
-        aux_vet[i] = aux_vet[menor];
-        aux_vet[menor] = aux;
+        aux = aux_vect[i];
+        aux_vect[i] = aux_vect[smallest];
+        aux_vect[smallest] = aux;
 
     }
-    return aux_vet;
+    return aux_vect;
 }
